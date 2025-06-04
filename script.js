648f073a7a7a1db4e11fd4b38b4ceb81b9982f3b
Emiliyan247 <emiliyankazandzhiev@gmail.com>
2025-06-04 13:45:00 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const animalAnimations = {
+  lion: '<img src="https://media.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif" class="animal-gif" alt="Lion" />',
+  elephant: '<img src="https://media.giphy.com/media/QBd2kLB5qDmysEXre9/giphy.gif" class="animal-gif" alt="Elephant" />',
+  eagle: '<img src="https://media.giphy.com/media/Y4pAQv58ETJgRwoLxj/giphy.gif" class="animal-gif" alt="Eagle" />',
+  snake: '<img src="https://media.giphy.com/media/3o6ZsU1D2Fu3My4xFC/giphy.gif" class="animal-gif" alt="Snake" />',
+  dog: '<img src="https://media.giphy.com/media/13CoXDiaCcCoyk/giphy.gif" class="animal-gif" alt="Dog" />'
+};
